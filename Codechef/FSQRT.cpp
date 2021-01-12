b317ff7e621329138334bed30448460b1884fcbe
gittykrish <kantkrishna663@gmail.com>
2021-01-12 11:59:19 +0530

    Finding Square Roots

---
+++ Codechef/FSQRT.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int res=sqrt(n);
+	    cout<<res<<"\n";
+	}
+	return 0;
+}
