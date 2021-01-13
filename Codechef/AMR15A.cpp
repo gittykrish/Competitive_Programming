9aa408e46013100a28922e9eb3ee2144c9428b87
gittykrish <kantkrishna663@gmail.com>
2021-01-14 01:15:06 +0530

    Mahasena

---
+++ Codechef/AMR15A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,count_even=0,count_odd=0;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a%2==0)count_even++;
+	    else count_odd++;
+	}
+	if(count_even>count_odd) cout<<"READY FOR BATTLE\n";
+	else cout<<"NOT READY\n";
+	return 0;
+}
