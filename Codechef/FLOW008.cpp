d6cf1fb52df4e221318091efc49cc4daa4ee8d0c
gittykrish <kantkrishna663@gmail.com>
2021-01-13 10:27:33 +0530

    Valid Triangle

---
+++ Codechef/FLOW008.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n<10){
+	        cout<<"Thanks for helping Chef!\n";
+	    }else{
+	        cout<<"-1\n";
+	    }
+	}
+	return 0;
+}
