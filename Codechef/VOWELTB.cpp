38d393c88d1614af54cbb29db4511c91b97ad5e7
gittykrish <kantkrishna663@gmail.com>
2021-01-16 22:47:46 +0530

    Is it Vowel or Consonent

---
+++ Codechef/VOWELTB.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+   char ch;
+   cin>>ch;
+   if(ch=='A'|| ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+       cout<<"Vowel";
+   }else{
+       cout<<"Consonant";
+   }
+   
+	return 0;
+}
+
