2fd57d9fd959f577d549daea7d21b67a9a749f54
gittykrish <kantkrishna663@gmail.com>
2021-01-18 12:19:50 +0530

    Tanu and headBOB

--- Codechef/HEADBOB.cpp
+++ Codechef/HEADBOB.cpp
@@ -1,23 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-string headbob(string str){
-    int n=str.size();
+string headbob(string str , int n){
+  
+     string res="NOT SURE";
     for(int i=0;i<n;i++){
         if(str[i]!='N'){
-            if(str[i]=='Y') return "NOT INDAIN";
-            else return "INDIAN";
+            if(str[i]=='I'){
+                res="INDIAN";
+                break;
+            }
+            else 
+            { 
+                res="NOT INDIAN";
+                break;
+            }
         }
+        
     }
-    return "NOT SURE";
+    return res;
 }
 int main() {
 	// your code goes here
 	int t;
 	cin>>t;
 	while(t--){
+	    int n;
+	    cin>>n;
 	    string str,res;
 	    cin>>str;
-	    res=headbob(str);
+	    res=headbob(str,n);
 	    cout<<res<<"\n";
 	}
 	return 0;
