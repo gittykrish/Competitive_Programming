7f8cad0e06179385a5c8f914943eff0b532bb119
gittykrish <kantkrishna663@gmail.com>
2021-01-19 23:46:12 +0530

    Baba and His Blessings

---
+++ Codechef/COG2003.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            v.push_back(a);
+        }
+        cout<<*max_element(v.begin(),v.end())<<"\n";
+    }
+    return 0;
+}
