c529ef509e286a97413880536573844a5ccd8ed5
gittykrish <kantkrishna663@gmail.com>
2021-01-21 23:49:09 +0530

    Fruit Basket

---
+++ Codechef/COW3A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    int a,b,c,d;
+    while(t--){
+    cin>>a>>b>>c>>d;
+    cout<<d-b<<" "<<d-c<<" "<<d-a<<"\n";
+    }
+	return 0;
+}
