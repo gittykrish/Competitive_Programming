dee8fbd052af668f2cfe6ae958cd46ed6aea4cc9
gittykrish <kantkrishna663@gmail.com>
2021-01-22 23:52:54 +0530

    balsa for three

---
+++ Codechef/BFTT.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+ int t,i,rem;
+ long long int n,temp; 
+ scanf("%d",&t);
+ 
+ for(i=1;i<=t;i++)
+ {
+     scanf("%lld",&n);
+        
+     int count=0;
+     
+     while(count!=3)
+     {
+       n++;
+        temp=n;
+        count=0;
+                
+        while(temp!=0)
+         {
+            rem=temp%10;
+            
+            temp=temp/10;
+            
+            if(rem==3)
+             {
+               count++;
+             } 
+            if(count==3)
+             {
+               break;
+             }
+         }
+      
+     }
+ 
+ printf("%lld\n",n);
+ 
+ }
+
+
+return 0;
+}
