8be3d58ceb98bdfc6632fad17532e69ed7ada364
Shri Krishna Tiwari <61533713+gittykrish@users.noreply.github.com>
2024-03-04 12:49:11 +0530

    Update ADAKING.cpp for renaming var t

--- Codechef/ADAKING.cpp
+++ Codechef/ADAKING.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
+    int tc;
+    cin>>tc;
+    while(tc--){
         int k,a=0,m=9;
         cin>>k;
         if(k%8==0){
